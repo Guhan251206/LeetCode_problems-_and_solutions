b3cf5b96ffb7b1ed36552795cf43da9a1133abeb
Guhan <guhan251206@gmail.com>
2026-06-23 22:32:27 +0530

    242. Valid Anagram

---
+++ String/Valid Anagram.py
@@ -0,0 +1,14 @@
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        a={}
+        for i in s:
+            a[i]=a.get(i,0)+1
+        for i in t:
+            if i not in a or a[i]==0:
+                return False
+            a[i]-=1
+        return True
+       
