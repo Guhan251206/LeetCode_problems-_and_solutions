d57239e4d0b5f6a94a67d499590530865ea118c0
Guhan <guhan251206@gmail.com>
2026-06-29 14:16:46 +0530

    70. Climbing Stairs

---
+++ Math/Climbing Stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n==0 or n==1:
+            return 1
+        c=0
+        a=0
+        b=1
+        for i in range(2,n+2):
+            c=a+b
+            a=b
+            b=c
+        return c
