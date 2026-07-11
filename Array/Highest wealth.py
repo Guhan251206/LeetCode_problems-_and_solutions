a9e8486d8d46a50505717b775056a1a9d2e6ff50
Guhan s <guhan251206@gmail.com>
2026-07-11 12:10:40 +0530

    Delete Array/Highest wealth.py

--- Array/Highest wealth.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        m=0
-        for i in range(len(accounts)):
-            s=sum(accounts[i])
-            if s>m:
-                m=s
-        return m
