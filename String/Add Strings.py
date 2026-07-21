32c2a1dfe38a078a1d0a7f08324315693dad6aa4
Guhan <guhan251206@gmail.com>
2026-07-21 22:20:01 +0530

    415. Add Strings

---
+++ String/Add Strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        i=len(num1)-1
+        j=len(num2)-1
+        a=[]
+        car=0
+        while i>=0 or j>=0 or car:
+            d1=ord(num1[i])-ord('0') if i>=0 else 0
+            d2=ord(num2[j])-ord('0') if j>=0 else 0
+            t=d1+d2+car
+            car=t//10
+            a.append(chr(t%10+ord('0')))
+            i-=1
+            j-=1
+        return "".join(a[::-1])
