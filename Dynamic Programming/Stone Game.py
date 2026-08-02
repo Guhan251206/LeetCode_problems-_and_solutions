8264459d5e028bb0ec975f7e2397781e75499891
Guhan <guhan251206@gmail.com>
2026-08-02 22:37:25 +0530

    877. Stone Game

---
+++ Dynamic Programming/Stone Game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        n=len(piles)
+        if n%2==0:
+            return True
+        dp = list(piles)
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                dp[j] = max(piles[i] - dp[j], piles[j] - dp[j - 1])
+        return dp[-1] >= 0
