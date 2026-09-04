7198071546d1dbccfcf02daf45c6f5c935d16d56
Guhan <guhan251206@gmail.com>
2026-09-04 11:09:02 +0530

    3903. Smallest Stable Index I

---
+++ Array/Smallest Stable Index I.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        for i in range(len(nums)):
+            a=max(nums[:i+1])
+            b=min(nums[i:])
+            if a-b<=k:
+                return i
+        return -1
